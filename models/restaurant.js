a1a7815c3ed0f32bba0282d5c3ed3ac97997283e
wei0328 <weichchang0328@gmail.com>
2023-11-06 01:41:03 -0500

    feat: add mongoose

---
+++ models/restaurant.js
@@ -0,0 +1,25 @@
+const mongoose=require('mongoose')
+const Schema=mongoose.Schema
+const restaurantSchema=new Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    name_en:{
+        type:String
+    },
+    category:{
+        type:String
+    },
+    image:{
+        type:String
+    },
+    location:{
+        type:String
+    },
+    phone:{
+        type:String
+    },
+
+})
+module.exports=mongoose.model('Restaurant',restaurantSchema)
